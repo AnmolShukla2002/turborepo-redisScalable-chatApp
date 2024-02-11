118af32c598b3a3bf5970b617471ca90134f45bb
Anmol Shukla <anmol.28422@gmail.com>
2024-02-11 16:29:58 +0530

    server side message publish using Aiven redis

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,13 +1,26 @@
 "use client";
+import { useState } from "react";
+import { useSocket } from "../context/SocketProvider";
 import classes from "./page.module.css";
 
 export default function Page() {
+  const { sendMessage, messages } = useSocket();
+  const [message, setMessage] = useState("");
+
   return (
     <div>
-      <div>ALL Messages will appear here</div>
       <div>
-        <input className={classes["chat-input"]} placeholder="Message..." />
-        <button>Send</button>
+        <input
+          onChange={(e) => setMessage(e.target.value)}
+          className={classes["chat-input"]}
+          placeholder="Message..."
+        />
+        <button
+          onClick={(e) => sendMessage(message)}
+          className={classes["button"]}
+        >
+          Send
+        </button>
       </div>
     </div>
   );
